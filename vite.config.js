24b9a403b553d0fbfdf54804c4e67d1ea94c415a
Sajith-Mathavaraja <mathavarajasajith@gmail.com>
2026-08-19 23:42:49 +0530

    Configure GitHub Pages automated deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/J-landscaping-llc-Liverpool/',
   plugins: [react()],
   build: {
     target: 'es2020',
